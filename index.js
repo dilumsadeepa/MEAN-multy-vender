5ca2dfb1d33c36e14474191e24c24c8f5cf26160
Dilum Sadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2023-01-21 21:31:32 +0530

    updated

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+  cors: {origin : '*'}
+});
+const cors = require('cors')
+const path = require('path')
+const mongoose = require('mongoose')
+
+const bodyParser = require('body-parser')
+
+const port = 3000;
+
+mongoose
+  .connect('mongodb+srv://DilumSadeepa:Dilum1234@cluster0.wuewbgs.mongodb.net/?retryWrites=true&w=majority')
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch((err) => {
+    console.error('Error connecting to mongo', err.reason)
+  })
+
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+)
+const userRoute = require('./routes/user.route')
+// const taskRoute = require('./routes/task.route')
+// app.use(express.static(path.join(__dirname, '../../client')))
+// app.use('/', express.static(path.join(__dirname, '../../client')))
+app.use('/user', userRoute)
+// app.use('/task', taskRoute)
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('message', (message) => {
+    console.log(message);
+    io.emit('message', `${message}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected!');
+  });
+});
+
+httpServer.listen(port, () => console.log(`listening on port ${port}`)); 
